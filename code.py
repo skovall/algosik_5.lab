8db599f50cd25aa709090e1e7029b46883f1d6e6
skovall <stetskovall@gmail.com>
2026-06-08 09:11:48 +0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+    
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert_recursive(node.left, value)
+        elif value > node.value:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert_recursive(node.right, value)
+    
+    def search(self, target):
+        return self._search_recursive(self.root, target)
+    
+    def _search_recursive(self, node, target):
+        if node is None:
+            return False
+        if node.value == target:
+            return True
+        elif target < node.value:
+            return self._search_recursive(node.left, target)
+        else:
+            return self._search_recursive(node.right, target)
+    
+    def inorder(self):
+        result = []
+        self._inorder_recursive(self.root, result)
+        return result
+    
+    def _inorder_recursive(self, node, result):
+        if node is not None:
+            self._inorder_recursive(node.left, result)
+            result.append(node.value)
+            self._inorder_recursive(node.right, result)
+    
+    def size(self):
+        return self._size_recursive(self.root)
+    
+    def _size_recursive(self, node):
+        if node is None:
+            return 0
+        return 1 + self._size_recursive(node.left) + self._size_recursive(node.right)
